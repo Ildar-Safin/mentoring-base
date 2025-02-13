6ae84cddcd2165d22a5b97898daaea9fe05c8f9b
Ildar Safin <ildarka94@mail.ru>
2025-02-13 19:30:00 +0300

    refactor

--- src/app/users-list/user-card/user-card.component.ts
+++ src/app/users-list/user-card/user-card.component.ts
@@ -6,14 +6,13 @@ import {DeleteUserDialogComponent} from "../delete-user-dialog/delete-user-dialo
 import {MatSnackBar, MatSnackBarModule} from "@angular/material/snack-bar";
 import {MatCardModule} from "@angular/material/card";
 import {MatButtonModule} from "@angular/material/button";
-import {CustomUpperCasePipe} from "../../pipes/upper-case.pipe";
 import {UpperCasePipe} from "@angular/common";
 
 @Component({
   selector: 'app-user-card',
   templateUrl: './user-card.component.html',
   styleUrl: './user-card.component.scss',
-  imports: [MatDialogModule, MatSnackBarModule, MatCardModule, MatButtonModule, CustomUpperCasePipe, UpperCasePipe ],
+  imports: [MatDialogModule, MatSnackBarModule, MatCardModule, MatButtonModule, UpperCasePipe ],
   standalone: true
 })
 
